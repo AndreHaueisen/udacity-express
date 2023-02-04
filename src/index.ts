b49136849a2358eb89d565366cf9ebaf2ed36063
andrehaueisen <andrehaueisen@gmail.com>
2023-02-04 16:02:24 -0300

    Adds first custom middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes/index';
+import logger from './utils/logger';
 
 const app = express();
 const port = 3000;
 
+app.all('*', logger);
 app.use('/api', routes);
 
 app.listen(port, function () {
--- src/routes/api/teachers.ts
+++ src/routes/api/teachers.ts
@@ -6,4 +6,8 @@ teachers.get('/', (_, res) => {
   res.send('Teachers route');
 });
 
+teachers.get('/1', (_, res) => {
+  res.send('Here is the first teacher');
+});
+
 export default teachers;
